49389334abd64740946248ddb57cb604af9c112d
Precious Idubor <preciousidubor@gmail.com>
2023-02-08 15:46:31 -0800

    Gather API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    user = requests.get(url)
+    name = user.json().get("name")
+
+    url = "https://jsonplaceholder.typicode.com/todos/"
+    todo = requests.get(url, params={"userId": id}).json()
+
+    t_complete = 0
+    t_list = ""
+
+    for task in todo:
+        if (task.get("completed") is True):
+            t_complete += 1
+            t_list += "\t " + task.get("title") + "\n"
+
+    print("Employee {} is done with tasks({}/{}):".format(name,
+                                                          t_complete,
+                                                          len(todo)))
+
+    print(t_list[:-1])
