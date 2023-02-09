a0a883225aec95530098bc8c7ea67631d59f048c
Precious Idubor <preciousidubor@gmail.com>
2023-02-08 18:39:19 -0800

    test

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-"""Exports info to JSON file"""
+"""Gathering API data"""
 import json
 import requests
+import sys
+
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
 
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+    url = "https://jsonplaceholder.typicode.com/users"
+    user = requests.get(url)
+
+    file_name = "todo_all_employees.json"
+
+    for u in user.json():
+        t_list = []
+        id = u.get("id")
+        url = "https://jsonplaceholder.typicode.com/todos"
+        todo = requests.get(url, params={"userId": id}).json()
+        name = u.get("username")
+        for task in todo:
+            task_dict = {}
+            task_dict["username"] = name
+            task_dict["task"] = task.get("title")
+            task_dict["completed"] = task.get("completed")
+            t_list.append(task_dict)
+
+        data = {"{}".format(id): t_list}
+        with open(file_name, "a") as fd:
+            json.dump(data, fd)
