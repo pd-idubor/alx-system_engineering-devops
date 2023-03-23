e3b52d2930ed867ba20e1e767455f9274a2b5235
Precious Idubor <preciousidubor@gmail.com>
2023-03-23 14:33:24 -0700

    Subreddit count

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers in a subreddit"""
+
+    header = {'User-Agent':  'Chrome/100.0.4896.88'}
+    url = "https://api.reddit.com/r/{}/about/".format(subreddit)
+    response = requests.get(url, headers=header)
+    if response.status_code == 200:
+        sub = response.json()["data"]["subscribers"]
+    else:
+        sub = 0
+    return sub
