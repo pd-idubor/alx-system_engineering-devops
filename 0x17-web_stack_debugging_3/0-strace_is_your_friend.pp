0aa7ac5a9b7b29e384ef1206ae1cb3b0e918953c
pd-idubor <105555834+pd-idubor@users.noreply.github.com>
2023-04-19 13:20:15 +0100

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-#debugging today a Wordpress website running on a LAMP stack
-
-exec { 'fix-wordpress':
-  command => "/bin/sed -i /var/www/html/wp-settings.php \
-  -e 's/class-wp-locale.phpp/class-wp-locale.php/'"
-}
