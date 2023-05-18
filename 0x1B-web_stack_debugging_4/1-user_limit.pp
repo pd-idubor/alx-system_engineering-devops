80326196ba8c20b8249a6359cc5b61cd6ac2de90
Precious Idubor <preciousidubor@gmail.com>
2023-05-18 14:29:11 -0700

    Maximum open files

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,9 @@
+# Set new values for the limit of max open files for all the system
+
+exec { 'change-max-open-files-hard-limit':
+  command => "/bin/sed -i /etc/security/limits.conf -e 's/hard nofile [0-9]/hard nofile 4000/g'"
+}
+
+exec { 'change-max-open-files-soft-limit':
+  command => "/bin/sed -i /etc/security/limits.conf -e 's/soft nofile [0-9]/soft nofile 3000/g'"
+}
